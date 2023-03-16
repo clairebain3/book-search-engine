90a742bb16ab5b4ea5249a2b2fface1c7f9d1f17
Claire Bain <claire.bain3@gmail.com>
2023-03-16 13:15:22 -0700

    heroku debugging

--- server/config/connection.js
+++ server/config/connection.js
@@ -5,6 +5,11 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false,
+}).then(()=>{
+  console.log("mongodb is connected");
+}).catch((error)=>{
+  console.log("mondb not connected");
+  console.log(error);
 });
 
 console.log(process.env.MONGODB_URI)
