72f6e8133f1d21fc48a91310f9ca18e0e904731a
Claire Bain <claire.bain3@gmail.com>
2023-03-16 12:57:01 -0700

    heroku testing

--- server/config/connection.js
+++ server/config/connection.js
@@ -7,4 +7,5 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
   useFindAndModify: false,
 });
 
+console.log(process.env.MONGODB_URI)
 module.exports = mongoose.connection;
