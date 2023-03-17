b9297ff8d392ea963c7d812172d8506bb685c9cd
Claire Bain <claire.bain3@gmail.com>
2023-03-16 17:38:58 -0700

    adds README.md

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # book-search-engine
+
+Description: This is a full stack application that allows users to search for a book, save it to their profile, and delete the saved book. It also has login and signup functionality. I used starter code to convert this app from a RESTful API to a graphQL API built with Apollo server. The app has a REACT front end.
+
+Installation: N/A
+
+Usage: Please visit this link: https://floating-brook-93855.herokuapp.com/ 
+
+Credits N/A
+
+License Please refer to the license in the repo
+
+![Picture of webpage](assets/Book-Search.png)
--- server/config/connection.js
+++ server/config/connection.js
@@ -12,5 +12,16 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
   console.log(error);
 });
 
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://clairebain3:mypassword>@cluster0.lv5w54x.mongodb.net/?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+
+
 console.log(process.env.MONGODB_URI)
 module.exports = mongoose.connection;
