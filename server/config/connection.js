c570f12ede35caab6e1f08bbe4ae6e6ae5653342
Claire Bain <claire.bain3@gmail.com>
2023-03-17 11:04:18 -0700

    trying to fix heroku issues

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI, {
-  // process.env.MONGODB_URI ||   
-  // 'mongodb+srv://clairebain3:mypassword@cluster0.lv5w54x.mongodb.net/googlebook?retryWrites=true&w=majority'
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+
 useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
@@ -14,14 +13,7 @@ useNewUrlParser: true,
   console.log(error);
 });
 
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-// const uri = "mongodb+srv://clairebain3:mypassword>@cluster0.lv5w54x.mongodb.net/?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
+
 
 console.log(process.env.MONGODB_URI)
 
